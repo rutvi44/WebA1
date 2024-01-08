bcc934bd81797422142cef1d79542a35d56cc272
Rutvi <126711988+rutvi44@users.noreply.github.com>
2024-01-08 13:04:57 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+//Name: Rutvi Mistry (Assignment1RM)
+//Revision History: 10/08/2023, Rutvi Mistry Created
+
+using Assignment1RM.Entities;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("BPMeasurementRMistry");
+builder.Services.AddDbContext<MeasurementDbContext>(options => options.UseSqlServer(connectionString));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
